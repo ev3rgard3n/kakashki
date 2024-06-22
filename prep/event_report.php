c43f1feff7cd8999b4191fc713992125a94e7b68
ev3rgard3n <ev3rgarden.off@gmail.com>
2024-06-22 08:12:11 +0400

    eqweqweq

---
+++ prep/event_report.php
@@ -0,0 +1,76 @@
+<?php
+
+    require_once "./header.php";
+    require_once "../connect.php"
+
+?>
+  <main>
+
+    <section class="py-5">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-6">
+            <h1>Молодёжный форум «ИННОВА  2024</h1>
+            <p class="lead">С 17 по 20 июня 2024 года пройдет Всероссийский форум студентов и молодых специалистов «ИННОВА—2024».
+              <br><br>
+                В этом году на форуме будут представлены следующие тематические проектные треки:
+                <br> - цифровое проектирование и моделирование;
+                <br>
+                - новые материалы;
+                <br>
+                - аддитивные технологии;
+                <br>
+                - CNC-технологии и гибридные технологии;
+                <br>
+                - промышленная сенсорика;
+                <br>
+                - технологии робототехники;
+                <br>
+                - информационные системы управления предприятием;
+                <br>
+                - Big Data;
+                <br>
+                - индустриальный интернет.
+                <br>
+              
+             </p>
+              
+                    
+          </div>
+          <div class="col-md-6">
+            <img
+              src="https://sun9-50.userapi.com/impg/4UpyDiqePgAANg1hFYf11FKUTBKdZJQtKT0--A/LxkyI_q9dMc.jpg?size=960x923&quality=95&sign=4ccda2a52b02b43c5c1d3498a6251210&c_uniq_tag=mcpTwfYyRNZG3X4E9v6HcJmcICRnR00sgwhLIXLVSNI&type=album"
+              style="height: 300px; width: 500px;" alt="Товар" class="img-fluid">
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="py-5 draggable">
+      <div class="container">
+        <div class="row justify-content-center text-center">
+          <div class="col-md-3">
+            <h1 class="text-gradient text-primary">500+</h1>
+            <h5 class="mt-3">Участников</h5>
+            <p>было зарегистрировано</p>
+          </div>
+          <div class="col-md-3">
+            <h1 class="text-gradient text-primary">324</h1>
+            <h5 class="mt-3">Участника</h5>
+            <p>пришло на мероприятие</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </main>
+
+  <?php
+  require_once "./footer.php";
+  ?>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
